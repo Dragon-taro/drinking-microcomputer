f2399eaf4eeb85ece45e866bac4b5266ae939e77
Dragon-taro <dragon.taro.lioil@gmail.com>
2019-09-19 14:50:12 +0900

    デプロイ可能に

--- server/main.go
+++ server/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"log"
+
 	"github.com/Dragon-taro/drinking-microcomputer/server/infrastructure/firestore"
 	"github.com/Dragon-taro/drinking-microcomputer/server/infrastructure/router"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	e := echo.New()
 	fc, err := firestore.NewFirestoreClient()
 	if err != nil {
