2d5a3c8166ddc13584231ab246315dfea606aed7
Dragon-taro <dragon.taro.lioil@gmail.com>
2019-09-21 15:41:38 +0900

    dataの表示

--- front/src/components/page/Index/presentation.tsx
+++ front/src/components/page/Index/presentation.tsx
@@ -1,13 +1,31 @@
 import * as React from "react";
 import { Props } from "./type";
 
-const Index: React.FunctionComponent<Props> = ({ finishParty, getData }) => {
+const Index: React.FunctionComponent<Props> = ({
+  finishParty,
+  getData,
+  data
+}) => {
   React.useEffect(() => {
     getData();
   }, []);
 
   return (
     <div>
+      <div>
+        {data &&
+          data.map(d => {
+            return (
+              <div>
+                <div>作成日時:{d.createdAt}</div>
+                <div>量: {d.amount}</div>
+                <div>合計量: {d.totalAmount}</div>
+                <div>前回との差分: {d.diff}</div>
+                <hr />
+              </div>
+            );
+          })}
+      </div>
       <div>
         <button onClick={finishParty}>飲み会を終了する</button>
       </div>
